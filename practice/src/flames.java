846c1ee8b80299019ef2ee0c321a6d50b773bb5c
SUBASH B K <subashbalasubramaniamm@gmail.com>
2023-01-29 10:27:05 +0530

    update

--- practice/src/flames.java
+++ practice/src/flames.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
-
+)
 public class flames {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
